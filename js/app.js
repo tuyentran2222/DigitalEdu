3eb1e2e421be6d7efc4cfa7205b36bdf1d442ff8
tuyentran2222 <tranvantuyen02082000@gmail.com>
2022-04-15 02:31:07 +0700

    add resposive header

--- js/app.js
+++ js/app.js
@@ -2,12 +2,28 @@ import header from "./components/header.js";
 import footer from "./components/footer.js";
 const headerElement = document.getElementById('header');
 const footerElement = document.getElementById('footer');
-headerElement.innerHTML = header;
+// headerElement.innerHTML = header;
 footerElement.innerHTML = footer;
 
 const closeVideoBtn = document.getElementById('close-video');
 const overlayVideo = document.getElementById('overlay');
 const videoPlayIcon = document.querySelector('.video__play-icon');
+const toggleBtn = document.querySelector('.menu__icon');
+const toggleNav = document.querySelector('.header__navbar');
+
+if (toggleBtn) {
+    toggleBtn.addEventListener('click', () => {
+        headerElement.classList.toggle('header-active');
+        if (headerElement.classList.contains('header-active')) {
+            if (window.scrollY <= 50) headerElement.classList.add('dark');
+            toggleNav.style.display = 'flex';
+        }
+       else toggleNav.style.display = 'none';
+        console.log(window.scrollY)
+
+    })
+}
+
 console.log(videoPlayIcon)
 if (videoPlayIcon) {
     videoPlayIcon.addEventListener('click', ()=> {
@@ -24,7 +40,13 @@ if (closeVideoBtn) {
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
     const header = document.getElementById('header');
-    if (this.scrollY >=50 ) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+    if (this.scrollY >=50 ) {
+        header.classList.add('scroll-header');
+        header.classList.remove('dark')
+    } else {
+        if (header.classList.contains('header-active')) header.classList.add('dark')
+        header.classList.remove('scroll-header');
+    }
 }
 window.addEventListener('scroll', scrollHeader);
 
